8820afe62cfaa3625cf1a5b597a8f3eba87b6bbf
ARYAN38-eng <124352330+ARYAN38-eng@users.noreply.github.com>
2025-06-12 22:10:18 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,12 +4,12 @@ const SECRET_KEY = process.env.SECRET_KEY;
 
 exports.authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) return res.sendStatus(401);
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
-    if (err) return res.sendStatus(403); // Invalid token
+    if (err) return res.sendStatus(403); 
     req.user = user;
     next();
   });
